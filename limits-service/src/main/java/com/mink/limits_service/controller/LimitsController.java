82e39c5778437db1da416b6a7e167abce3d1276b
DESKTOP-0FQEQD0\batha <bathackcf@gmail.com>
2024-06-02 14:27:45 +0700

    added limits properties

--- limits-service/src/main/java/com/mink/limits_service/controller/LimitsController.java
+++ limits-service/src/main/java/com/mink/limits_service/controller/LimitsController.java
@@ -3,15 +3,22 @@ package com.mink.limits_service.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mink.limits_service.bean.Limits;
+import com.mink.limits_service.configuration.Configuration;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class LimitsController {
 
+    private Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 100);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 
 }
